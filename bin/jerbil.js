4c3b2a13ca47c82e18c25acdbf026b6c4c3854a6
Daniel Winterstein <daniel@good-loop.com>
2020-05-17 11:59:03 +0100

    npm cli

--- bin/jerbil.js
+++ bin/jerbil.js
@@ -3,6 +3,9 @@ const shell = require("shelljs");
 const https = require('https');
 const fs = require('fs');
 
+const jar = 'jerbil-all.jar';
+const cmd = 'jerbil';
+
 //console.log(process.argv);
 const args = [...process.argv];
 // shidt node command, script path
@@ -10,22 +13,24 @@ args.shift();
 const dirPath = args.shift();
 console.log(dirPath, args);
 
-const jarExists = fs.existsSync('jerbil-all.jar');
+
+const jarExists = fs.existsSync(jar);
 
 if ( ! jarExists || args[0]==="-update" || args[0]==="--update") {
-	console.log("Downloading latest jerbil-all.jar...");
+	console.log("Downloading latest "+jar+"...");
 	// cross-platform download -- sadly not as 100% reliable as wget
-	const file = fs.createWriteStream("jerbil-all.jar");
-	const request = https.get("https://www.winterwell.com/software/downloads/jerbil-all.jar", function(response) {
+	const file = fs.createWriteStream(jar);
+	const request = https.get("https://www.winterwell.com/software/downloads/"+jar, function(response) {
 		  response.pipe(file);
 		  console.log("...Download complete.");	
 		  if (args[0]==="-update" || args[0]==="--update") {
 			return; // dont go on to build a site
 		}
-		console.log("Note: If you get a message 'Invalid or corrupt jarfile' - Try re-running jerbil.");
-		console.log("If it repeats, run `jerbil -update` to download the jar again.");	
+		console.log("Note: If you get a message 'Invalid or corrupt jarfile' - Try re-running "+cmd);
+		console.log("If it repeats, run `"+cmd+" -update` to download the jar again.");	
 	});
 	return;
 }
 
-shell.exec("java -jar jerbil-all.jar "+args.join(" "));
+shell.exec("java -jar "+jar" "+args.join(" "));
+
