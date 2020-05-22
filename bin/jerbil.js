e4adc5ec56c97475429dbd1663141a01c61ed0d4
Daniel Winterstein <daniel@good-loop.com>
2020-05-22 13:12:40 +0100

    local/bin

--- bin/jerbil.js
+++ bin/jerbil.js
@@ -6,31 +6,51 @@ const fs = require('fs');
 const jar = 'jerbil-all.jar';
 const cmd = 'jerbil';
 
+let jarFile;
+// Can we store the jar in a sort of canonical place? ~/bin
+// https://unix.stackexchange.com/posts/36874/revisions
+if (process.env.HOME && fs.existsSync(process.env.HOME)) {
+	let jardir = process.env.HOME+'/bin';
+	if ( ! fs.existsSync(jardir)) {
+		fs.mkdirSync(jardir);
+	}
+	jarFile = jardir+"/"+jar;
+} else {
+	jarFile = jar;
+}
+
 //console.log(process.argv);
 const args = [...process.argv];
 // shidt node command, script path
 args.shift();	
 const dirPath = args.shift();
-console.log(dirPath, args);
 
+const ocmd = "java -jar "+jarFile+" "+args.join(" ");
 
-const jarExists = fs.existsSync(jar);
+const jarExists = fs.existsSync(jarFile);
 
 if ( ! jarExists || args[0]==="-update" || args[0]==="--update") {
 	console.log("Downloading latest "+jar+"...");
 	// cross-platform download -- sadly not as 100% reliable as wget
-	const file = fs.createWriteStream(jar);
+	const file = fs.createWriteStream(jarFile);
 	const request = https.get("https://www.winterwell.com/software/downloads/"+jar, function(response) {
-		  response.pipe(file);
-		  console.log("...Download complete.");	
-		  if (args[0]==="-update" || args[0]==="--update") {
-			return; // dont go on to build a site
-		}
-		console.log("Note: If you get a message 'Invalid or corrupt jarfile' - Try re-running "+cmd);
-		console.log("If it repeats, run `"+cmd+" -update` to download the jar again.");	
+		  let rp = response.pipe(file);
+		  // TODO process the pipe and detect "all done"
+		//   console.log("...Download complete. "+Object.keys(rp));
+		//   if (args[0]==="-update" || args[0]==="--update") {
+			// dont go on to build a site
+		return;
+		// }
+		// // do it
+		// console.log(ocmd);
+		// shell.exec(ocmd);
+		// return;
 	});
+	console.log("Note: If you get a message 'Invalid or corrupt jarfile' - Try re-running "+cmd+" -update to download the jar again.");
 	return;
-}
+} else {
 
-shell.exec("java -jar "+jar+" "+args.join(" "));
-
+	// do it now
+	console.log(ocmd);
+	shell.exec(ocmd);
+};
